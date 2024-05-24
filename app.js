213ab89fc478d26478054a8622d6519c124d06b3
JOSE MARCOS <joseluismarcosramos@outlook.com>
2024-05-24 17:03:56 -0500

    Resolución

--- app.js
+++ app.js
@@ -1,17 +1,26 @@
 'use strict'
 const GI = {
-	createElement (tag, atributes) {
-		let element = document
-		return document.createElement(tag)
-	}
+  createElement(tag, atributes) {
+    let element = document
+    return document.createElement(tag)
+  }
 }
 
 
 const to_do = [
-    'Alimentar al perro',
-    'Estudiar para prueba escrita',
-    'Pagar taxes',
-    'Pasear a perro',
-    'Comprar entradas para el cine',
+  'Alimentar al perro',
+  'Estudiar para prueba escrita',
+  'Pagar taxes',
+  'Pasear a perro',
+  'Comprar entradas para el cine',
 ]
+
+document.getElementById('to-do').innerHTML = to_do.map((item, index) => `<li>
+  <label id="to_do${index}">${item}</label>
+  <input type="checkbox" name="to_do${index}" onchange="textoEstilo(event)" />
+</li>`).join('')
+
+function textoEstilo(e) {
+  document.getElementById(e.target.name).classList.toggle('estilo-todo')
+}
 
